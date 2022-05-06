abc0099c7c98e4b6b5d308be8bf5d8e90f23b459
vass.gergo <vass.gergo@e-vista.hu>
2022-05-06 13:18:42 +0200

    renamed functions for wp plugin team :)

--- tooltip-akos.php
+++ tooltip-akos.php
@@ -8,16 +8,16 @@
 * Author URI: https://profiles.wordpress.org/csomorelwood/
 **/
 
-function register_the_unlimited_power_of_akos_raised_by_csomorelwood() {
+function TBA_register_styles_and_i_need_to_use_custom_function_names_for_better_debugging_but_also_the_last_name_was_pretty_cool() {
   wp_register_style('akos_style_lol', plugins_url('assets/css/style.css',__FILE__ ));
   wp_enqueue_style('akos_style_lol');
   
 }
-add_action( 'init','register_the_unlimited_power_of_akos_raised_by_csomorelwood');
+add_action( 'init','TBA_register_styles_and_i_need_to_use_custom_function_names_for_better_debugging_but_also_the_last_name_was_pretty_cool');
 
 
-function magical_script_from_the_god_himself_akos() {
+function TBA_register_scripts_then_drink_a_cup_of_coffee_while_writing_a_plugin_under_ten_minutes() {
   wp_register_script('akos_script_lol', plugin_dir_url( __FILE__ ) . '/assets/js/akos.js' );
   wp_enqueue_script( 'akos_script_lol');
 }
-add_action('init', 'magical_script_from_the_god_himself_akos');
+add_action('init', 'TBA_register_scripts_then_drink_a_cup_of_coffee_while_writing_a_plugin_under_ten_minutes');
